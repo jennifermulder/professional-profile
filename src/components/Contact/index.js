168266db36eea4080e7f7deb49940ca28ab5e5be
J Mulder <jlmulder2014@gmail.com>
2020-10-04 17:46:27 -0700

    contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
+  //hook
+  // clear input field on the component loading
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+  const { name, email, message } = formState;
+  //initial state is an empty string
+  const [errorMessage, setErrorMessage] = useState('');
+
+  function handleChange(e) {
+    //target email <input> element
+    if (e.target.name === 'email') {
+      //store return value in "isValid"
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      // isValid conditional statement
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      //check "message" and name "form" values
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    //only update if valid
+    if (!errorMessage) {
+      //... spread operator to retain the other key-value pairs in the object (otherwise would only contain name:value)
+      //uses setFormState function to update formState value for the name property
+      //.name refers to name attribute not name key
+      //use [] to create dynamic property names
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+  // console.log(formState);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
+  // JSX
+  //htmlFor = for 
+  //defaultValue={formState.name} --- assign initial state values to the input fields in the DOM
+  //default value pulling from formState
+  //on Change (ex. keystroke in field), fire handleChange function
+  // && if true, will render second part
   return (
     <section>
       <h1 data-testid="h1tag">Contact me</h1>
@@ -8,6 +58,27 @@ function ContactForm() {
           <h4 htmlFor="email">Email address: jlmulder2014@gmail.com</h4>
           <h4 htmlFor="email">Phone Number: 510.766.6583</h4>
 
+      <h1 data-testid="h1tag">Send Email</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button type="submit" data-testid="button">Submit</button>
+      </form>
     </section>
   )
 }
