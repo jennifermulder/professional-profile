e405720f7b4086b01b80f16b1d3f03a23e394a3d
J Mulder <jlmulder2014@gmail.com>
2020-10-06 20:43:26 -0700

    active

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,7 @@ function About(props) {
   return (
     <section>
       <h1 id="about">About Me</h1>
-      <img src={profileImage} className="my-2" style={{ width: "25%" }} alt="Jennifer Mulder" />
+      <img src={profileImage} className="my-2" style={{ width: "75%" }} alt="Jennifer Mulder" />
       <p> I am a Full-Stack Web developer looking to contribute to software functionality in a creative workspace. As a recent recipient of a certificate in full stack development from the University of California, Berkeley, I have obtained newly practiced skills including: Javascript, NodeJS, and MySQL. With a collective 6 years of experience in both public and industry accounting, I have the necessary capabilities to be an effective contributing team member to any project. In public accounting, I gained the ability to effectively communicate with all management levels on the client side, as well as my team. I am a courageous communicator when it comes to presenting/ suggesting more efficient ways to perform processes as I am self-motivated to always find a better solution. When working on my recent group project to build a full stack application utilizing express and sequelize to build an interactive blog site, I played a leadership role in assessing the skills of those that I was working with and assigning the best fit to each role, while also maintaining organization and adherence to acceptance criteria. I am excited to utilize my skills in an efficient, fast-paced workplace that is equally driven to come up with the next best thing.
 </p>
     </section>
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -6,34 +6,34 @@ function Nav(props) {
   const {
     setPageSelected
   } = props;
-  
-console.log(props, "coming from nav");
-  return (
-      <nav>
-        <ul className="flex-row">
-          <li className="mx-2">
-            <a data-testid="about" href="#about" onClick={() => setPageSelected("about me")}>
-              About me
-            </a>
-          </li>
-          <li className="mx-2">
-            <a data-testid="portfolio" href="#portfolio" onClick={() => setPageSelected("portfolio")}>
-              Portfolio
-            </a>
-          </li>
-          <li className="mx-2">
-            <a data-testid="contact" href="#contact" onClick={() => setPageSelected("contact")}>
-              Contact
-            </a>
-          </li>
-          <li className="mx-2">
-            <a data-testid="resume" href="#resume" onClick={() => setPageSelected("resume")}>
-              Resume
-            </a>
-          </li>
 
-        </ul>
-      </nav>
+  console.log(props, "coming from nav");
+  return (
+    <nav>
+      <ul className="flex-row">
+        <li className={`mx-2 ${true ? 'active' : ''}`}>
+          <a data-testid="about" href="#about" onClick={() => setPageSelected("about me")}>
+            About me
+            </a>
+        </li>
+        <li className="mx-2">
+          <a data-testid="portfolio" href="#portfolio" onClick={() => setPageSelected("portfolio")}>
+            Portfolio
+            </a>
+        </li>
+        <li className="mx-2">
+          <a data-testid="contact" href="#contact" onClick={() => setPageSelected("contact")}>
+            Contact
+            </a>
+        </li>
+        <li className="mx-2">
+          <a data-testid="resume" href="#resume" onClick={() => setPageSelected("resume")}>
+            Resume
+            </a>
+        </li>
+
+      </ul>
+    </nav>
   );
 }
 
